7f9c8925adf1fa0e83e5c3ebb0420020b70cf163
Vivek Khatri <vivek@esamudaay.com>
2022-12-03 15:08:39 +0530

    Remove prompt chaining

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,45 +6,21 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix =
-    `
-Write me some motivation by reading my problem below
-
-Problem:
-`
-
+const basePromptPrefix = "Write me some motivation by reading my problem ";
 const generateAction = async (req, res) => {
+    // Run first prompt
     console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
     const baseCompletion = await openai.createCompletion({
         model: 'text-davinci-003',
         prompt: `${basePromptPrefix}${req.body.userInput}`,
-        temperature: 0.8,
+        temperature: 0.7,
         max_tokens: 250,
     });
 
     const basePromptOutput = baseCompletion.data.choices.pop();
 
-    // I build Prompt #2.
-    const secondPrompt =
-        `
-  Make the above motivational content sound more positive
-
-  `
-
-    // I call the OpenAI API a second time with Prompt #2
-    const secondPromptCompletion = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${secondPrompt}`,
-        temperature: 0.9,
-        max_tokens: 600,
-    });
-
-    // Get the output
-    const secondPromptOutput = secondPromptCompletion.data.choices.pop();
-
-    // Send over the Prompt #2's output to our UI instead of Prompt #1's.
-    res.status(200).json({ output: secondPromptOutput });
+    res.status(200).json({ output: basePromptOutput });
 };
 
 export default generateAction;
